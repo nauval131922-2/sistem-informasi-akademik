3cc237032f45f3fc65e7da1f99e5dd31cbe06736
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-07-11 23:25:49 +0700

    asd

---
+++ resources/views/backend/pekerjaan/edit.blade.php
@@ -0,0 +1,131 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+
+
+            <p class="card-title-desc" style="border-bottom: 1px solid rgb(161,179,191)">Lengkapi form
+                berikut untuk mengubah {{ $title }}.</p>
+            <form method="POST" id="formUbahData">
+                @csrf
+                <div class="row mb-3">
+                    <label for="pekerjaan" class="col-sm-2 col-form-label">pekerjaan</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Masukkan pekerjaan"
+                            id="pekerjaan" name="pekerjaan"
+                            value="{{ $pekerjaan->pekerjaan ?? old('pekerjaan') }}" required>
+                        <div class="mt-2">
+                            {{-- @error('pekerjaan')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror --}}
+                            <span class="text-danger error-text pekerjaan_error"></span>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-info waves-effect waves-light">
+                    <i class="ri-refresh-line align-middle me-1"></i>
+                    <span style="vertical-align: middle">Update</span>
+                </button>
+            </form>
+
+        </div>
+    </div>
+</div> <!-- end col -->
+
+<script>
+    $(document).ready(function() {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+    });
+
+    $('#gambar').change(function(e) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#showImage').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    function clearFile() {
+        $('#gambar').val('');
+        $('#showImage').attr('src', '');
+        $('#gambarPreview').val('');
+    }
+
+
+
+    // update data
+    $('#formUbahData').on('submit', function(e) {
+        e.preventDefault();
+
+        var id = {{ $pekerjaan->id }};
+        let formData = new FormData($('#formUbahData')[0]);
+
+        $.ajax({
+            url: '/pekerjaan/update/' + id,
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            beforeSend: function() {
+                $(document).find('span.error-text').text('');
+            },
+            success: function(response) {
+
+                if (response.status == 'error') {
+                    $.each(response.message, function(prefix, val) {
+                        $('span.' + prefix + '_error').text(val[0]);
+                    });
+                } else if (response.status == 'error2') {
+                    toastr.warning(response.message, "", {
+                        "closeButton": true,
+                        "debug": false,
+                        "newestOnTop": false,
+                        "progressBar": false,
+                        "positionClass": "toast-top-right",
+                        "preventDuplicates": false,
+                        "onclick": null,
+                        "showDuration": "100",
+                        "hideDuration": "100",
+                        "timeOut": "5000",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                    });
+                } else {
+
+                    // toastr success message
+                    toastr.success(response.message, "", {
+                        "closeButton": false,
+                        "debug": false,
+                        "newestOnTop": true,
+                        "progressBar": false,
+                        "positionClass": "toast-top-right",
+                        "preventDuplicates": false,
+                        "onclick": null,
+                        "showDuration": "100",
+                        "hideDuration": "100",
+                        "timeOut": "1500",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                    });
+
+                    // hide modal
+                    $('#exampleModalScrollable').modal('hide');
+
+                    // fetch data
+                    fetchData();
+                }
+            }
+        })
+
+    })
+</script>
