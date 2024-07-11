24b3076c14e463f65b390c9e182948e1c8c83adf
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-07-11 23:19:11 +0700

    $table->foreignId('id_diterima_di_kelas_for_siswa')->nullable()->constrained('kelas')->onUpdate('cascade')->onDelete('set null');

---
+++ database/migrations/2024_07_07_215412_add_columns_to_siswas_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('siswas', function (Blueprint $table) {
+            $table->foreignId('id_diterima_di_kelas_for_siswa')->nullable()->constrained('kelas')->onUpdate('cascade')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('siswas', function (Blueprint $table) {
+            $table->dropColumn('id_diterima_di_kelas_for_siswa');
+        });
+    }
+};
