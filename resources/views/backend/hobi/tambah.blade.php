c42e980d5c01db43d82b88f13d20f203027abdb6
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-07-11 23:22:48 +0700

    blade hobi

---
+++ resources/views/backend/hobi/tambah.blade.php
@@ -0,0 +1,136 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+
+
+            <p class="card-title-desc" style="border-bottom: 1px solid rgb(161,179,191)">Lengkapi form
+                berikut untuk menambah {{ $title }}.</p>
+            <form method="POST" id="formTambahData">
+                @csrf
+                <div class="row mb-3">
+                    <label for="hobi" class="col-sm-2 col-form-label">Hobi</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Masukkan Hobi"
+                            id="hobi" name="hobi" value="{{ old('hobi') }}" required>
+                        <div class="mt-2">
+                            {{-- @error('hobi')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror --}}
+                            <span class="text-danger error-text hobi_error"></span>
+                        </div>
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-info waves-effect waves-light">
+                    <i class="ri-save-3-line align-middle me-1"></i>
+                    <span style="vertical-align: middle">Simpan</span>
+                </button>
+            </form>
+
+        </div>
+    </div>
+</div> <!-- end col -->
+
+<script>
+    $('#gambar').change(function(e) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#showImage').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    function clearFile() {
+        $('#gambar').val('');
+        $('#showImage').attr('src', '');
+        $('#gambarPreview').val('');
+    }
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    // insert data
+    $('#formTambahData').on('submit', function(e) {
+        e.preventDefault();
+
+        let formData = new FormData($('#formTambahData')[0]);
+
+        $.ajax({
+            url: '{{ route('hobi-simpan') }}',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            beforeSend: function() {
+                $(document).find('span.error-text').text('');
+            },
+            success: function(response) {
+
+                if (response.status == 'error') {
+                    $.each(response.message, function(prefix, val) {
+                        $('span.' + prefix + '_error').text(val[0]);
+                    });
+                } else if (response.status == 'error2') {
+                    toastr.warning(response.message, "", {
+                        "closeButton": true,
+                        "debug": false,
+                        "newestOnTop": false,
+                        "progressBar": false,
+                        "positionClass": "toast-top-right",
+                        "preventDuplicates": false,
+                        "onclick": null,
+                        "showDuration": "100",
+                        "hideDuration": "100",
+                        "timeOut": "5000",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                    });
+                } else {
+
+                    // toastr success message
+                    toastr.success(response.message, "", {
+                        "closeButton": false,
+                        "debug": false,
+                        "newestOnTop": true,
+                        "progressBar": false,
+                        "positionClass": "toast-top-right",
+                        "preventDuplicates": false,
+                        "onclick": null,
+                        "showDuration": "100",
+                        "hideDuration": "100",
+                        "timeOut": "1500",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                    });
+
+                    // reset form
+                    $('#formTambahData')[0].reset();
+
+                    // clear image
+                    clearFile();
+
+                    // fetch data
+                    fetchData();
+                }
+
+            }
+        })
+
+    })
+
+
+
+    $(document).ready(function() {
+
+
+    });
+</script>
