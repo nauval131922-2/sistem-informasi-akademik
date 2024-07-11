368a7da295cc19a8fb0e72020637debd45e9e4e9
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-07-11 23:10:52 +0700

    factory

---
+++ database/factories/HobiFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Hobi>
+ */
+class HobiFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
