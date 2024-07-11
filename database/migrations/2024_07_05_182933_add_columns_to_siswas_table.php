3a6d952681d7d7480fd817388f079316e5776815
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-07-11 23:16:40 +0700

    $table->string('ijazah')->nullable();

---
+++ database/migrations/2024_07_05_182933_add_columns_to_siswas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('siswas', function (Blueprint $table) {
+            // tambah kolom ijazah
+            $table->string('ijazah')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('siswas', function (Blueprint $table) {
+            // menghapus kolom ijazah
+            $table->dropColumn('ijazah');
+        });
+    }
+};
