f5dcbecd328f3e0d84ac71ab2760002821281cc5
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-08-17 21:53:18 +0700

    tambah bintang untuk yang wajib diisi

--- resources/views/backend/hobi/tambah.blade.php
+++ resources/views/backend/hobi/tambah.blade.php
@@ -4,18 +4,15 @@
 
 
             <p class="card-title-desc" style="border-bottom: 1px solid rgb(161,179,191)">Lengkapi form
-                berikut untuk menambah {{ $title }}.</p>
+                berikut untuk menambah {{ $title }}. <small class="text-danger">* Harus diisi</small></p>
             <form method="POST" id="formTambahData">
                 @csrf
                 <div class="row mb-3">
-                    <label for="hobi" class="col-sm-2 col-form-label">Hobi</label>
+                    <label for="hobi" class="col-sm-2 col-form-label">Hobi <span class="text-danger">*</span></label>
                     <div class="col-sm-10">
                         <input class="form-control" type="text" placeholder="Masukkan Hobi"
                             id="hobi" name="hobi" value="{{ old('hobi') }}" required>
                         <div class="mt-2">
-                            {{-- @error('hobi')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror --}}
                             <span class="text-danger error-text hobi_error"></span>
                         </div>
                     </div>
