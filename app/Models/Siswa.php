561029daf304249efcf1e13c4fc95ee1445f0f2d
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-08-17 21:45:16 +0700

    tambah fn pekerjaanAyah, pekerjaanIbu, pekerjaanWali, user

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -14,4 +14,19 @@ class Siswa extends Model
     public function kelas(){
         return $this->belongsTo(Kelas::class, 'id_kelas', 'id');
     }
+
+    public function pekerjaanAyah(){
+        return $this->belongsTo(Pekerjaan::class, 'id_pekerjaan_ayah_for_siswa', 'id');
+    }
+
+    public function pekerjaanIbu(){
+        return $this->belongsTo(Pekerjaan::class, 'id_pekerjaan_ibu_for_siswa', 'id');
+    }
+    public function pekerjaanWali(){
+        return $this->belongsTo(Pekerjaan::class, 'id_pekerjaan_wali_for_siswa', 'id');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class, 'id_user_for_siswa', 'id');
+    }
 }
